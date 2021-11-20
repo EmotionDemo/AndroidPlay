659ba11b4f8c15d639e7880891ef1e047bf62b1c
lifenghua <lifenghua@newbeiyang.com>
2021-11-20 17:10:04 +0800

    1、添加agentWebView

--- app/src/main/java/com/example/test/activity/AndroidActivity.kt
+++ app/src/main/java/com/example/test/activity/AndroidActivity.kt
@@ -1,12 +1,39 @@
 package com.example.test.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.test.R
+import android.widget.RelativeLayout
+import com.example.test.R.id
+import com.example.test.R.layout
+import com.just.agentweb.AgentWeb
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
-class AndroidActivity : AppCompatActivity() {
+class AndroidActivity : BaseActivity() {
+    private lateinit var rlContent : RelativeLayout;
+    private lateinit var executor: Executor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_android)
+        setContentView(layout.activity_android)
+        rlContent = findViewById(id.rlContent)
+        executor = Executors.newSingleThreadExecutor()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        executor.execute {
+            /*AgentWeb awvContentView = AgentWeb.with(this)
+                .setAgentWebParent(rlContent,RelativeLayout.LayoutParams(-1,1))
+                .useDefaultIndicator()
+                .createAgentWeb()
+                .ready()
+                .go();*/
+            /*var awvContentView:AgentWeb = AgentWeb.with(this)
+                .setAgentWebParent(rlContent,RelativeLayout.LayoutParams(-1,1))*/
+//            var awvContentView: AgentWeb = AgentWeb.with(this)
+
+
+
+
+        }
     }
 }
